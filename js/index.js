d996571a1d5fbba859e6cf091b3889e43d5a2e01
Pratik <php231@nyu.edu>
2015-04-21 12:28:20 -0400

    Initial Page

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function cycleBackgrounds() {
+    var index = 0;
+
+    $imageEls = $('.toggle-image'); // Get the images to be cycled.
+
+    setInterval(function () {
+        // Get the next index.  If at end, restart to the beginning.
+        index = index + 1 < $imageEls.length ? index + 1 : 0;
+        // Show the next image.
+        $imageEls.eq(index).addClass('show');
+        // Hide the previous image.
+        $imageEls.eq(index - 1).removeClass('show');
+
+    }, 3000);
+};
+
+// Document Ready.
+$(function () {
+    cycleBackgrounds();
+});
+
+
+
+$('#myTabs a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+});
